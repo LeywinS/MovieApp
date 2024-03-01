0f7b742e32fff81577bdf116d5257f48e137f2d9
LeywinS <backary.sou@gmail.com>
2024-03-01 19:16:02 +0100

    third commit

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,17 +1,16 @@
 "use client";
+
 import { useEffect } from "react";
-
-function error({ error, reset }) {
-  console.log(error);
-
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong. Please try again later.</h1>
-      <button className="hover:text-amber-500" onClick={() => reset()}>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
         Try Again
       </button>
     </div>
   );
 }
-
-export default error;
