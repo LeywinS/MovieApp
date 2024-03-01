43fb89284950a2d9ad403fcfccd8cb7f6c6e8d08
LeywinS <backary.sou@gmail.com>
2024-03-01 18:49:38 +0100

    deployment test1

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -2,9 +2,8 @@
 import { useEffect } from "react";
 
 function error({ error, reset }) {
-  useEffect(() => {
-    console.log(error);
-  }, [error]);
+  console.log(error);
+
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong. Please try again later.</h1>
